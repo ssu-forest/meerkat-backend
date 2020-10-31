82dda23a0ff74868b6778f716d68638c60f1eb41
cloudjun <junnetwork7@gmail.com>
2020-10-31 13:18:04 +0900

    수정사항 반영

--- src/services/board.service.ts
+++ src/services/board.service.ts
@@ -3,28 +3,31 @@ import { CreateUserDto } from '../dtos/users.dto';
 import HttpException from '../exceptions/HttpException';
 import { User } from '../interfaces/users.interface';
 import userModel from '../models/users.model';
-import dbService from '../lib/db';
+import dbService from '../lib/postgre.db';
 
 class BoardService {
 
-  public async boardlist(param: Array<string>): Promise<any> {
+  public async boardlist(param: Array<number>): Promise<any> {
+    const categoryList: Array<number> = [0,1,2,3,4]; // 0: 공지 / 1: 자유 / 2: 취미 / 3: 번개 / 4: 홍보
+    if(categoryList.indexOf(param[0]) == -1) throw new HttpException(602, '없는 게시판입니다.');
+
     const sqlValue = param;
     const sql = `
       SELECT 
-        id 
-      FROM USER 
-      WHERE id = ?`;
+        board_id,
+        board_title, 
+        board_contents, 
+        category, 
+        write_dt, 
+        modify_dt, 
+        view_count, 
+        like_count, 
+        user_id 
+      FROM ssu_forest.meerkat_board
+      WHERE category = $1`;
 
     const db = new dbService();
     const queryData = await db.query(sql, sqlValue);
-    if(queryData.length == 0){
-      throw new HttpException(602, '없는 대학 정보를 조회했어요.');
-    }
-
-    queryData[0]['delYn']  = queryData[0]['delYn'] ? true : false ; // mysql bool은 0/1 로 리턴함에 따라 치환
-    if(queryData[0]['delYn'] == true){ //관리자의 경우 그대로 정보를 전달하지만 관리자가 아님에 따라 결과 리턴
-      throw new HttpException(602, '없는 대학 정보를 조회했어요.2');
-    }
 
     return queryData;
   }
